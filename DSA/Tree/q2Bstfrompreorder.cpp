8723f4d694e907a5891db12a0b04909c50e52886
JITENDRA-DUBEY <jitendrakumardubeybas@gmail.com>
2022-04-24 15:51:02 +0530

    j

--- DSA/Tree/q2Bstfrompreorder.cpp
+++ DSA/Tree/q2Bstfrompreorder.cpp
@@ -38,35 +38,35 @@ int Stack::topOfstack()
 }
 void Stack::push(Node*data)
 {
-    if(top==sz-1)
-    cout<<"stack overflow "<<endl;
-    else{
+    //if(top==sz-1)
+    //cout<<"stack overflow "<<endl;
+   // else{
         top=top+1;
         S[top]=data;
-    }
+   // }
 }
 void Stack :: display()
 {
-    if(top==-1)
-    cout<<"stack is empty "<<endl;
-    else{
+    //if(top==-1)
+    //cout<<"stack is empty "<<endl;
+    //else{
     int i=top;
     while(i>=0)
     {
         cout<<S[i]<<"\n";i--;
     }
     cout<<"\n";
-}
+//}
 }
 Node* Stack::Pop()
 {
     Node* x=NULL;
-    if(top==-1)
-    cout<<"stack underflow"<<endl;
-    else{
+    //if(top==-1)
+    //cout<<"stack underflow"<<endl;
+    //else{
        x=S[top];
        top=top-1;
-    }
+   // }
     return x;
 }
 class Bst
@@ -76,7 +76,7 @@ class Bst
     void preorder(Node*p);
 };
 void Bst::create(int pre[],int n)
-{//if p->data<data logic before inseryion lchild first pushg p in to stack than creat lchild of p
+{//if p->data<data logic before insertion lchild first pushg p in to stack than creat lchild of p
   //if(p->data<data&&data<topstack data than assing p->rchild =t and don't need to push data else p=popo();)
      Node*t=NULL,*p;
      Stack st(n);int i=0;
@@ -115,9 +115,10 @@ void Bst::preorder(Node*p)
    
     if(p!=NULL){
        
-         
+       
         preorder(p->lchild);
-        preorder(p->rchild);cout<<p->data<<" ";
+         cout<<p->data<<" "; 
+        preorder(p->rchild);
        
     }
 }
